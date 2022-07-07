2cdfcf029447aa19931d24ab038306c6a48f16b0
luaramb <arambarrilucia@gmail.com>
2022-07-07 19:49:46 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import CardPersonaje from './components/cardPersonaje/cardPersonaje';
 
 function App() {
+  const [personajes, setPersonajes] = useState([])
+
+  const getCharacter = async () => {
+    const response = await axios.get('https://rickandmortyapi.com/api/character')
+    setPersonajes(response.data.results)
+  }
+
+  console.log(personajes)
+
+  useEffect(() => {
+    getCharacter()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+    <div className='container'>
+      <p> rick and morty</p>
+      {
+        personajes.map(personaje => (
+          <CardPersonaje  key={personaje.id} {...personaje}/>
+        ))
+      }
     </div>
   );
 }
